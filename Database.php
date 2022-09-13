64eaf844e8f595e4548f6b7926be0d0fbbaf176f
asharomet <sharometal@gmail.com>
2022-09-13 14:37:01 +0300

    first commit

---
+++ Database.php
@@ -0,0 +1,42 @@
+<?php
+
+class Database {
+
+    private $link;
+
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    public function connect()
+    {
+        $config = require_once 'config.php';
+        $dsn = 'mysql:host='. $config['host'] .';dbname=' .$config['db_name']. ';charset=' .$config['charset'];
+        $this->link = new PDO($dsn, $config['username'], $config['password']);
+        return $this;
+    }
+
+    public function execute($sql)
+    {
+        $sth = $this->link->prepare($sql);
+        return $sth->execute();
+    }
+
+    public function query($sql)
+    {
+        $sth = $this->prepare($sql);
+        $sth->execute();
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+        if($result === false) {
+            return [];
+        }
+        return $result;
+    }
+
+    public function prepare($sql)
+    {
+        return $sth = $this->link->prepare($sql);
+    }
+
+}
